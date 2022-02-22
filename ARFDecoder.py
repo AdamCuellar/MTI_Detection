911b5fc73cfe9f0b7e916346ba80daf9a5ec0f01
Adam Cuellar <cuellar.adam@gmail.com>
2022-02-22 13:30:02 -0500

    Fix symlinks

--- ARFDecoder.py
+++ ARFDecoder.py
@@ -1,1 +1,76 @@
-../ARFDecoder.py
+import os
+import sys
+import numpy as np
+
+class ARFDecoder():
+    def __init__(self, arfPath):
+        self.arfPath = arfPath
+        self.byteSwapFn = self.swapBytesLittle if sys.byteorder == 'little' else self.swapBytesBig
+        self.fid = None
+        try:
+             self.fid = open(arfPath, "rb")
+        except:
+            print("Error reading file: ", arfPath)
+
+        self.fileInfo = self.getInfo()
+
+    def swapBytesLittle(self, frame):
+        frame.byteswap(inplace=True)
+        return
+
+    def swapBytesBig(self, frame):
+        # arf files should be in big endian
+        return
+
+    def getInfo(self):
+        headerDict = None
+        if self.fid is None: return None
+
+        header = np.fromfile(self.fid, dtype=np.dtype(np.uint32), count=8)
+        self.byteSwapFn(header)
+
+        # check the arf file is valid
+        if header[0] == 3149642413:
+            headerDict = {"magic_num":header[0], "version":header[1], "height":header[2], "width":header[3],
+                "type":header[4], "numFrames":header[5], "offset":header[6], "flags":header[7]}
+
+        return headerDict
+
+    def getHumanReadableType(self, type, flags):
+        types = ["GRAY8", "GRAY16_SIGNED", "GRAY16_SIGNED", "GRAY32_INT",
+                 "GRAY16_UNSIGNED", "CHECKBIT", "GRAY32_FLOAT", "GRAY64_FLOAT"]
+
+        if "CHECKBIT" in types[type]:
+            if flags & 128 == 128:
+                return "ARGB"
+            else:
+                return "GRAY32_UNSIGNED"
+        else:
+            return types[type]
+
+    def getFrameRetrievalInfo(self):
+        type = self.fileInfo["type"]
+        dataTypeMap = { 0 : np.uint8,
+                        1 : np.int16,
+                        2 : np.int16,
+                        3 : np.int16,
+                        4 : np.int32,
+                        5 : np.uint16,
+                        6 : np.uint32,
+                        7 : np.float32,
+                        8 : np.double }
+
+        return dataTypeMap[type]
+
+    def readFrame(self, frameNum):
+        bitType = np.dtype(self.getFrameRetrievalInfo())
+        self.fid.seek(self.fileInfo['offset'] + (frameNum * bitType.itemsize * self.fileInfo['width'] * self.fileInfo['height']), 0)
+
+        frame = np.fromfile(self.fid, dtype=bitType, count=(self.fileInfo["width"] * self.fileInfo["height"]))
+
+        if frame.size < (self.fileInfo["width"] * self.fileInfo["height"]): return np.empty((0,0)) # reached end of file
+
+        self.byteSwapFn(frame)
+        frame.shape = (self.fileInfo["height"], self.fileInfo["width"])
+
+        return frame
--- ARX.py
+++ ARX.py
@@ -1,1 +1,153 @@
-ARX.py
+# import jax
+import numpy as np
+# import jax.numpy as jnp
+
+""" 
+Reed-Xiaoli Anomaly Detection Algorithms
+Each one of the algorithms below can be found in the following paper:
+https://www.umbc.edu/rssipl/pdf/TGRS/tgrs.anomaly/40tgrs06-chang-proof.pdf
+
+Each function expects a numpy array of shape (height, width, channels).
+This can be multiple frames from a video to detect movers or a hyperspectral cube.
+
+Any missing algorithms will be added at a later date (utd, arx-utd, etc)
+"""
+
+# def arxGPU(input):
+#     assert False, "ARX GPU NOT WORKING"
+#     h, w, ch = input.shape
+#     I = input.reshape((h * w, ch))
+#     I = I - jnp.mean(I)
+#     covMatrix = jnp.cov(I.T)
+#
+#     try:
+#         inv = jnp.linalg.inv(covMatrix)
+#     except np.linalg.LinAlgError:
+#         inv = jnp.linalg.pinv(covMatrix)
+#
+#     M = jnp.matmul(I, inv)
+#     rxScore = M.reshape(h, w, 1, ch) @ I.reshape(h, w, ch, 1)
+#     rxScore = jnp.squeeze(rxScore)
+#     return rxScore
+
+def arx(input, usePinv=False):
+    h, w, ch = input.shape
+    I = input.reshape((h*w, ch))
+    I = I - np.mean(I)
+    covMatrix = np.cov(I.T)
+
+    if not usePinv:
+        try:
+            inv = np.linalg.inv(covMatrix)
+        except np.linalg.LinAlgError:
+            inv = np.linalg.pinv(covMatrix)
+    else:
+        inv = np.linalg.pinv(covMatrix)
+
+    M = np.matmul(I, inv)
+    rxScore = M.reshape(h, w, 1, ch) @ I.reshape(h, w, ch, 1)
+    rxScore = np.squeeze(rxScore)
+    return rxScore
+
+def arxCorr(input, usePinv=False):
+    h, w, ch = input.shape
+    I = input.reshape((h*w, ch))
+    covMatrix = np.corrcoef(I.T)
+
+    if not usePinv:
+        try:
+            inv = np.linalg.inv(covMatrix)
+        except np.linalg.LinAlgError:
+            inv = np.linalg.pinv(covMatrix)
+    else:
+        inv = np.linalg.pinv(covMatrix)
+
+    M = np.matmul(I, inv)
+    rxScore = M.reshape(h, w, 1, ch) @ I.reshape(h, w, ch, 1)
+    rxScore = np.squeeze(rxScore)
+    return rxScore
+
+def normArx(input, usePinv=False):
+    h, w, ch = input.shape
+    I = input.reshape((h*w, ch))
+    I = I - np.mean(I)
+    I = I /  np.linalg.norm(I)
+    covMatrix = np.cov(I.T)
+
+    if not usePinv:
+        try:
+            inv = np.linalg.inv(covMatrix)
+        except np.linalg.LinAlgError:
+            inv = np.linalg.pinv(covMatrix)
+    else:
+        inv = np.linalg.pinv(covMatrix)
+
+    M = np.matmul(I, inv)
+    rxScore = M.reshape(h, w, 1, ch) @ I.reshape(h, w, ch, 1)
+    rxScore = np.squeeze(rxScore)
+    return rxScore
+
+def normArxCorr(input, usePinv=False):
+    h, w, ch = input.shape
+    I = input.reshape((h*w, ch))
+    I = I / np.linalg.norm(I)
+    covMatrix = np.corrcoef(I.T)
+
+    if not usePinv:
+        try:
+            inv = np.linalg.inv(covMatrix)
+        except np.linalg.LinAlgError:
+            inv = np.linalg.pinv(covMatrix)
+    else:
+        inv = np.linalg.pinv(covMatrix)
+
+    M = np.matmul(I, inv)
+    rxScore = M.reshape(h, w, 1, ch) @ I.reshape(h, w, ch, 1)
+    rxScore = np.squeeze(rxScore)
+    return rxScore
+
+def modifiedArx(input, usePinv=False):
+    h, w, ch = input.shape
+    I = input.reshape((h*w, ch))
+    I = I - np.mean(I)
+    covMatrix = np.cov(I.T)
+
+    if not usePinv:
+        try:
+            inv = np.linalg.inv(covMatrix)
+        except np.linalg.LinAlgError:
+            inv = np.linalg.pinv(covMatrix)
+    else:
+        inv = np.linalg.pinv(covMatrix)
+
+    M = np.matmul(I, inv)
+    rxScore = M.reshape(h, w, 1, ch) @ (I.reshape(h, w, ch, 1) / np.linalg.norm(I))
+    rxScore = np.squeeze(rxScore)
+    return rxScore
+
+def modifiedArxCorr(input, usePinv=False):
+    h, w, ch = input.shape
+    I = input.reshape((h*w, ch))
+    covMatrix = np.corrcoef(I.T)
+
+    if not usePinv:
+        try:
+            inv = np.linalg.inv(covMatrix)
+        except np.linalg.LinAlgError:
+            inv = np.linalg.pinv(covMatrix)
+    else:
+        inv = np.linalg.pinv(covMatrix)
+
+    M = np.matmul(I, inv)
+    rxScore = M.reshape(h, w, 1, ch) @ (I.reshape(h, w, ch, 1) / np.linalg.norm(I))
+    rxScore = np.squeeze(rxScore)
+    return rxScore
+
+if __name__ == "__main__":
+    fakeData = np.random.random((640,640,5))
+    rx = arx(fakeData)
+
+    # gpu version fails at the moment
+    # rxGPU = arxGPU(fakeData)
+
+    # print(" and GPU match: ", np.all(np.isclose(rx, rxGPU)))
